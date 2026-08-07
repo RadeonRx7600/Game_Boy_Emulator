c619583b9c39a5456b2ff600eed279d523384c86
RavioliYehaw <vinc.fernandez@outlook.fr>
2026-08-07 20:55:42 +0200

    Define Cpu_Regs structure in CPUcore.C test
    Added initial structure for CPU registers.

--- CPUcore.C
+++ CPUcore.C
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<stdint.h>
 
+/*
+    This is my co-work with Ravioli my Master Sama on the CPUcore part, it will be
+    difficult but a side kick always honor his master. YEHAW SIR 
+
+*/
+
+typedef struct Cpu_Regs {
+
+
+
+
+
+
+
+
+
+    
+    
+} REGISTER;
