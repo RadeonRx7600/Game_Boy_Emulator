db7ea9f6f3c1967a57d3b40dcda26299b856ef0b
Little Ravioli san <vinc.fernandez@outlook.fr>
2026-08-08 16:05:06 +0200

    Refactor CPU register structures with union

--- CPUcore.C
+++ CPUcore.C
@@ -7,16 +7,23 @@
 
 */
 
-typedef struct Cpu_Regs {
+typedef union {
+    struct {
+        // 8-bit Registers
+        uint8_t lo;
+        uint8_t hi;
+    };
+    uint16_t value;
+} Reg16;
 
+typedef struct {
+    // 8-bit Registers / 16-bit
+    Reg16 af;
+    Reg16 bc;
+    Reg16 de;
+    Reg16 hl;
 
-
-
-
-
-
-
-
-    
-    
-} REGISTER;
+    // 16-bit Registers
+    uint16_t sp;
+    uint16_t pc;
+} Registers;
